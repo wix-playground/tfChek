6bd7f6e90d18a5dd7e2ae82eda5751af248229da
Maksym Shkolnyi <maksymsh@wix.com>
2019-11-21 09:10:46 +0200

    Update entrypoint

--- tfChek.sh
+++ tfChek.sh
@@ -4,5 +4,9 @@ TFCHEK_PORT=$PORT
 export TFCHEK_PORT
 if [ -r "/configs/tfChek" ]; then
   source /configs/tfChek
+  echo "Environment variables has been sourced" 1>&2
+else
+  echo "WARNING!!! Environment variables has not been sourced" 1>&2
 fi
+echo "Launching $*" 1>&2
 ./tfChek "$@"
