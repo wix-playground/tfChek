d35a355044ffd714b42746fbeafe8b57fa3e25bb
Maksym Shkolnyi <maksymsh@wix.com>
2020-03-04 15:52:03 +0200

    Fix variable

--- tfChek.sh
+++ tfChek.sh
@@ -4,10 +4,10 @@ TFCHEK_PORT=$PORT
 export TFCHEK_PORT
 echo -e "\033[0;32mConfigured tfChek to listen to the port\033[0;35m $TFCHEK_PORT\033[0m"
 #Preparing keys
-mkdir ~/.ssh && chmod 700 ~/.ssh
+mkdir ${HOME}/.ssh && chmod 700 ${HOME}/.ssh
 cat /configs/id_rsa | sed 's~ RSA PRIVATE KEY~RSAPRIVATEKEY~g'| sed 's~[ ]~\n~g' | sed 's~RSAPRIVATEKEY~ RSA PRIVATE KEY~g' >${HOME}/.ssh/id_rsa
 chmod 400 ${HOME}/.ssh/id_rsa
-chown $USER ${MyHome}/.ssh/id_rsa
+chown $USER ${HOME}/.ssh/id_rsa
 echo -e "\033[0;32mSSH keys are ready\033[0m" && ( rm -f /configs/id_rsa || echo -e "\033[0;31mCannot remove id_rsa from configs directory\033[0m" )
 
 echo "Launching $*" 1>&2
