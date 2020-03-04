ea6d896378589f8e7ddc4dfac63bdbe01ffc8b85
Maksym Shkolnyi <maksymsh@wix.com>
2020-03-04 16:48:38 +0200

    Use sh

--- tfChek.sh
+++ tfChek.sh
@@ -1,14 +1,14 @@
-#!/bin/bash
+#!/bin/sh
 
 TFCHEK_PORT=$PORT
 export TFCHEK_PORT
 echo -e "\033[0;32mConfigured tfChek to listen to the port\033[0;35m $TFCHEK_PORT\033[0m"
 #Preparing keys
-#mkdir ~/.ssh && chmod 700 ~/.ssh
-#cat /configs/id_rsa | sed 's~ RSA PRIVATE KEY~RSAPRIVATEKEY~g'| sed 's~[ ]~\n~g' | sed 's~RSAPRIVATEKEY~ RSA PRIVATE KEY~g' > ~/.ssh/id_rsa
-#chmod 400 ~/.ssh/id_rsa
-#chown $(whoami) ~/.ssh/id_rsa
-#echo -e "\033[0;32mSSH keys are ready\033[0m"
+mkdir ~/.ssh && chmod 700 ~/.ssh
+cat /configs/id_rsa | sed 's~ RSA PRIVATE KEY~RSAPRIVATEKEY~g'| sed 's~[ ]~\n~g' | sed 's~RSAPRIVATEKEY~ RSA PRIVATE KEY~g' > ~/.ssh/id_rsa
+chmod 400 ~/.ssh/id_rsa
+chown $(whoami) ~/.ssh/id_rsa
+echo -e "\033[0;32mSSH keys are ready\033[0m"
 
 echo "Launching $*" 1>&2
 ./tfChek "$@"
