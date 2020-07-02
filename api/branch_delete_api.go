41f233e13320e2766a8f989026767033681ffa0a
Maksym Shkolnyi <maskimko@ukr.net>
2020-07-02 15:00:07 +0300

    Add API method (branch param)

--- api/branch_delete_api.go
+++ api/branch_delete_api.go
@@ -6,6 +6,7 @@ import (
 	"github.com/gorilla/mux"
 	"net/http"
 	"strconv"
+	"strings"
 	"tfChek/github"
 	"tfChek/misc"
 )
@@ -25,14 +26,42 @@ func DeleteCIBranch(w http.ResponseWriter, r *http.Request) {
 	branch, bok := v[misc.ApiBranchKey]
 	id, iok := v[misc.IdParam]
 	managers := github.GetAllManagers()
+	var err error
 	var taskId int
-	if iok {
-		taskId, err := strconv.Atoi(id)
+	if bok {
+		s := strings.Split(branch, "-")
+		if len(s) != 2 {
+			misc.Debugf("failed to split branch %s name onto 2 parts. Falling through to next parameter")
+		} else {
+			taskId, err = strconv.Atoi(s[1])
+			if err != nil {
+				misc.Debugf("cannot convert branch id %s to int. Error: %s", s[1], err)
+				w.WriteHeader(http.StatusNotAcceptable)
+				dr := &DeleteResponse{Error: fmt.Errorf("cannot convert branch %s to int. Error: %w", branch, err)}
+				mdr, err := json.Marshal(dr)
+				if err != nil {
+					w.Header().Add(misc.ContentTypeKey, misc.ContentTypeMarkdown)
+					misc.Debugf("cannot marshal response %v. Falling back to a plain text message. Error: %s", dr, err)
+					_, err := w.Write([]byte(fmt.Sprintf("cannot convert branch %s to int. Error: %s", branch, err)))
+					if err != nil {
+						misc.Debugf("cannot send a response. Error: %s", err)
+					}
+					return
+				}
+				_, err = w.Write(mdr)
+				if err != nil {
+					misc.Debugf("cannot send a response. Error: %s", err)
+				}
+				return
+			}
+		}
 
+	}
+	if iok {
+		taskId, err = strconv.Atoi(id)
 		if err != nil {
 			misc.Debugf("cannot convert branch id %s to int. Error: %s", id, err)
 			w.WriteHeader(http.StatusNotAcceptable)
-			//TODO: return Status here
 			dr := &DeleteResponse{Error: fmt.Errorf("cannot convert branch id %s to int. Error: %w", id, err)}
 			mdr, err := json.Marshal(dr)
 			if err != nil {
@@ -53,7 +82,8 @@ func DeleteCIBranch(w http.ResponseWriter, r *http.Request) {
 		branch = fmt.Sprintf("%s%s", misc.TaskPrefix, id)
 		misc.Debugf("%s=%s parameter has been provided in request, this is why %s parameter has been overridden to %s", misc.IdParam, id, misc.ApiBranchKey, branch)
 	}
-	//TODO: implement branch aram too
+
+	//Actual logic is here
 	status := make(map[string]*DeleteStatus)
 	for _, m := range managers {
 		err := m.GetClient().DeleteBranch(taskId)
